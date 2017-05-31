dc50627b7db1842268a3014d14e35984077245f4
Arman Yessymkanov <arman.yessymkanov@gmail.com>
2017-05-31 14:21:28 +0600

    Basic dev structure

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	cleanCSS = require('gulp-clean-css'),
+	concat = require('gulp-concat');
+
+gulp.task('styles', function() {
+	gulp.src()
+});
